d8f551c37f0c96f4935d57a53788bfaef46f38c3
Inga Eglaja <zalane.inga@gmail.com>
2020-08-18 16:03:26 +0300

    make tree pretty

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -1,6 +1,10 @@
 object Tree extends App {
   val Height = 3
-  for (currentHeight <- 1 until Height + 1) {
+  if (Height > 1) println(" " * (Height - 1) + "\ud83c\udf1f") //UTF symbol star 🌟
+  else println("\ud83c\udf32") //UTF symbol christmas tree 🌲
+  for (currentHeight <- 2 until Height + 1) {
     println(" " * (Height - currentHeight) + "*" * (currentHeight * 2 - 1))
   }
+
+
 }
